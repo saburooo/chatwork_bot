b1105ffe9843d5a360878fa2d66f33c2b3ccfc60
akihisha <65touhei@gmail.com>
2020-10-01 21:28:46 +0900

    無限ループでリプライまではいけた

--- app.py
+++ app.py
@@ -15,16 +15,16 @@ class MessageAnalize:
         このままクラスが増えすぎるのもまずいと思った。
         このクラスでメッセージとその他諸々を解析する。
         """
-        self.message_id : str = json_file["message_id"]
+        self.message_id  = json_file["message_id"]
         self.body : str = json_file["body"]
         self.name : str = json_file["account"]["name"]
-        self.account : str = json_file["account"]["account_id"]
+        self.account : int = json_file["account"]["account_id"]
         self.q_and_a = re.compile('質問|在宅(で|も|は).*?|これは')
 
     def greet(self) -> dict:
         return re.search("おはよう|こん(にち|ばん)[はわ]", self.body)
     
-    def parrot(self):
+    def parrot(self) -> dict:
         """
         オウム返し
         """
@@ -46,9 +46,9 @@ class MessageAnalize:
         if self.ngword():
             return "禁止ワードを入れるとかwww" + self.name + "君ちぃーっすwww"
         if self.greet():
-            return '[rp aid=' + self.account + ' to=' + self.message_id + ']' + self.name + "さん、こんにちは今日もご機嫌ですね",
-        if self.inword('(なん|何)だ(きみ|君)は(.*?!)'):
-            return "なんだチミ(君)はってか!? え!? なんだチミはってか！そうです、私が変なおじさんです"
+            return '[To:{}]'.format(self.account) + self.name + "さん、本日もよろしくおねがいします。",
+        if self.inword('(なん|何)だ(きみ|君)は'):
+            return "[To:{}]".format(self.account) +  "なんだチミ(君)はってか!? え!? なんだチミはってか！そうです、私が変なおじさんです"
 
 
 class ChatWork:
@@ -62,7 +62,7 @@ class ChatWork:
             "body":message
         }
         req = requests.post(self.message_url, headers=self.api_key, params=params)
-        req = requests.put(self.message_url + 'read', headers=self.api_key)
+        req = requests.put(self.message_url + '/read', headers=self.api_key)
     
     def chat_get(self) -> list:
         """
@@ -74,9 +74,10 @@ class ChatWork:
         except:
             json_file_list = {}
         if json_file_list != {}:
-            pprint(json_file_list[-1])
+            pprint(json_file_list)
         else:
-            print("ゲットできず")
+            pass
+        print(json_file_list)
         return json_file_list
     
     def get_keys(self) -> dict:
@@ -99,16 +100,14 @@ class ChatWork:
 @app.route('/', methods=['GET', 'POST'])
 def index():
     chat = ChatWork()
-    for c in chat.chat_get():
-        m = MessageAnalize(c)
-    
-    return chat.chat_post()
+    c_dict = {"今何を持っているのか":None}
+    while True:
+        for c in chat.chat_get():
+            m = MessageAnalize(c)
+            chat.chat_post(m.messagegenerate()) 
+            c_dict.update(c)
 
 
 if __name__ == "__main__":
-    chat = ChatWork()
-    for c in chat.chat_get():
-        m = MessageAnalize(c)
-        chat.chat_post(m.messagegenerate())
-    #app.debug=True
-    #app.run()
+    app.debug = True
+    app.run()
